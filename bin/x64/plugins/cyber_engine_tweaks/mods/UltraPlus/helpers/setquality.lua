a644311391b0a9f04224cca6ca0f54f662e3cc7a
Mark Glass <127671029+robber804@users.noreply.github.com>
2024-11-26 08:26:38 -0500

    Update setquality.lua
    move to setmode.lua

--- bin/x64/plugins/cyber_engine_tweaks/mods/UltraPlus/helpers/setquality.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/UltraPlus/helpers/setquality.lua
@@ -337,8 +337,6 @@ function Config.SetQuality(quality)
 			Cyberpunk.SetOption('Editor/ReSTIRGI', 'SpatialNumSamples', '0')
 			Cyberpunk.SetOption('Editor/ReSTIRGI', 'SpatialNumDisocclusionBoostSamples', '0')
 			Cyberpunk.SetOption('Editor/Denoising/ReBLUR/AmbientOcclusion', 'AntiFirefly', true)
-			Cyberpunk.SetOption('Editor/Denoising/ReBLUR/Direct', 'AntiFirefly', true)
-			Cyberpunk.SetOption('Editor/Denoising/ReBLUR/Indirect', 'AntiFirefly', true)
 			return
 		end
 
