8592e9dfc3637617836d92b7062b619618342e93
Mark Glass <127671029+robber804@users.noreply.github.com>
2025-01-02 08:54:07 -0500

    Update setvram.lua
    updated settings for 2.2 and some improvements to shadow batch and rain batch

--- bin/x64/plugins/cyber_engine_tweaks/mods/UltraPlus/helpers/setvram.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/UltraPlus/helpers/setvram.lua
@@ -9,8 +9,8 @@ function Config.SetVram(vram)
 	Logger.info('Configuring vram for', vram, 'GB')
 
 	if vram == Var.vram.OFF then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4000')
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '512')
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4096')			-- vanilla value is 4096
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '500')				-- vanilla value is 500
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '192')
 
@@ -30,7 +30,7 @@ function Config.SetVram(vram)
 	end
 
 	if vram == Var.vram.GB4 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4000')		-- how does this not OOM?
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4096')		
 		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '512')
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '128')
@@ -51,7 +51,7 @@ function Config.SetVram(vram)
 	end
 
 	if vram == Var.vram.GB6 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4000')		-- WAS 1500
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4096')		
 		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '512')
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '192')
@@ -71,8 +71,12 @@ function Config.SetVram(vram)
 	end
 
 	if vram == Var.vram.GB8 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4000')		-- WAS 2000
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '768')
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '8192')		
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
+
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '80')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '224')
 
@@ -98,8 +102,12 @@ function Config.SetVram(vram)
 	end
 
 	if vram == Var.vram.GB10 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4000')		-- WAS 2500
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '1024')
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '8192')		-- WAS 2500
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
+
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '80')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '224')
 
@@ -125,8 +133,12 @@ function Config.SetVram(vram)
 	end
 
 	if vram == Var.vram.GB12 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4000')		-- WAS 3000
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '1024')
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '8192')		-- WAS 3000
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
+
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '96')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '224')
 
@@ -152,8 +164,12 @@ function Config.SetVram(vram)
 	end
 
 	if vram == Var.vram.GB16 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4000')
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '1024')
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '16384')
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
+
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '112')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '256')
 
@@ -179,8 +195,12 @@ function Config.SetVram(vram)
 	end
 
 	if vram == Var.vram.GB20 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '5000')
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '1024')
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '16384')
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
+
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '128')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '320')
 
@@ -192,13 +212,13 @@ function Config.SetVram(vram)
 			Logger.info('    (NSGDD compatibility disabled)')
 			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '10.0')
 			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '20.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')						-- WAS 500
+			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')				-- WAS 500
 			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')		-- WAS 500
 		else
 			Logger.info('    (NSGDD compatibility enabled)')
 			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
 			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')						-- WAS 500
+			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')				-- WAS 500
 			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')		-- WAS 500
 		end
 
@@ -206,8 +226,12 @@ function Config.SetVram(vram)
 	end
 
 	if vram == Var.vram.GB24 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '6000')
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '1024')
+		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '16384')
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
+
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '128')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '320')
 
@@ -219,13 +243,13 @@ function Config.SetVram(vram)
 			Logger.info('    (NSGDD compatibility disabled)')
 			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '10.0')
 			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '20.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')						-- WAS 500
+			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')				-- WAS 500
 			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')		-- WAS 500
 		else
 			Logger.info('    (NSGDD compatibility enabled)')
 			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
 			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')						-- WAS 500
+			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')				-- WAS 500
 			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')		-- WAS 500
 		end
 
