0eed8b83a978079e3a8aeb761dbe4cd6fce2e9fa
Mark Glass <127671029+robber804@users.noreply.github.com>
2025-01-21 13:36:59 -0500

    Update setvram.lua
    removed settings that were specific to game editor

--- bin/x64/plugins/cyber_engine_tweaks/mods/UltraPlus/helpers/setvram.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/UltraPlus/helpers/setvram.lua
@@ -6,40 +6,20 @@ Config = {}
 Cyberpunk = require('helpers/Cyberpunk')
 
 function Config.SetVram(vram)
+
 	Logger.info('Configuring vram for', vram, 'GB')
 
 	if vram == Var.vram.OFF then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4096')		-- vanilla value is 4096
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '500')			-- was 768 / vanilla 500
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '4096.000000')				-- vanilla 4096
+		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '500')  				-- vanilla 500
 		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '400000')	-- vanilla 400000
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '300')  			-- was 768 vanilla 300
+		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '300')  					-- vanilla 300
 		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '200000')  	-- vanilla 200000
 
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '192')
 
 		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '300')
-		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')			-- vanilla value is 500
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')
-
-		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
-		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-
-		Logger.info('    (NSGDD compatibility force-enabled due to low VRAM)')
-		Var.settings.nsgddCompatible = true
-
-		return
-	end
-
-	if vram == Var.vram.GB4 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4096')		
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '512')
-		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')
-		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '128')
-
-		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')
 		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')
@@ -48,132 +28,96 @@ function Config.SetVram(vram)
 		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
 		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
 
-		Logger.info('    (NSGDD compatibility force-enabled due to low VRAM)')
-		Var.settings.nsgddCompatible = true
+	end
+
+	if vram == Var.vram.GB4 then
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '8192.000000')					-- vanilla 4096
+
+		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')				-- vanilla 64
+		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '192')			-- vanilla 192
+
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')		-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')			-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')			-- vanilla 1
+
+		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
+		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
+		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
+		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
 
 		return
 	end
 
 	if vram == Var.vram.GB6 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '4096')		
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '512')
-		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')
-		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '192')
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '8192.000000')					-- vanilla 4096
 
-		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')
-		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')
+		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')				-- vanilla 64
+		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '192')			-- vanilla 192
+
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')		-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')			-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')			-- vanilla 1
 
 		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
 		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-
-		Logger.info('    (NSGDD compatibility force-enabled due to low VRAM)')
+		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
+		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
 
 		return
 	end
 
 	if vram == Var.vram.GB8 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '8192')		
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '8192.000000')					-- vanilla 4096
 
-		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '80')
-		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '224')
+		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '64')				-- vanilla 64
+		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '192')			-- vanilla 192
 
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')		-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')			-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')			-- vanilla 1
 
-		if Var.settings.nsgddCompatible == false then
-			Logger.info('    (NSGDD compatibility disabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '10.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '20.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')
-		else
-			Logger.info('    (NSGDD compatibility enabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
-		end
+		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
+		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
+		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
+		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
 
-		return
 	end
 
 	if vram == Var.vram.GB10 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '8192')		-- WAS 2500
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
-
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '80')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '224')
 
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '8192.000000')					-- vanilla 4096
 
-		if Var.settings.nsgddCompatible == false then
-			Logger.info('    (NSGDD compatibility disabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '10.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '20.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')
-		else
-			Logger.info('    (NSGDD compatibility enabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')			--500/600 is probably enough for RT or PT but 700 is needed for raster
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
-		end
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')		-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')			-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')			-- vanilla 1
 
-		return
+		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
+		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
+		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
+		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
+
 	end
 
 	if vram == Var.vram.GB12 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '8192')		-- WAS 3000
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
-
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '8192.000000')					-- vanilla 4096
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '96')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '224')
 
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')
-		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '2')		-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')			-- vanilla 2
+		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')			-- vanilla 1
 
-		if Var.settings.nsgddCompatible == false then
-			Logger.info('    (NSGDD compatibility disabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '10.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '20.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')
-		else
-			Logger.info('    (NSGDD compatibility enabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
-		end
+		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
+		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
+		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
+		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
 
-		return
 	end
 
 	if vram == Var.vram.GB16 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '16384')
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
-
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '8192.000000')					-- vanilla 4096
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '112')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '256')
 
@@ -181,30 +125,15 @@ function Config.SetVram(vram)
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '2')
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '1')
 
-		if Var.settings.nsgddCompatible == false then
-			Logger.info('    (NSGDD compatibility disabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '10.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '20.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')
-		else
-			Logger.info('    (NSGDD compatibility enabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
-		end
+		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
+		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
+		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
+		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
 
-		return
 	end
 
 	if vram == Var.vram.GB20 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '16384')
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
-
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '8192.000000')					-- vanilla 4096
 		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '128')
 		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '320')
 
@@ -212,52 +141,26 @@ function Config.SetVram(vram)
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '3')
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '2')
 
-		if Var.settings.nsgddCompatible == false then
-			Logger.info('    (NSGDD compatibility disabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '10.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '20.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')				-- WAS 500
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')		-- WAS 500
-		else
-			Logger.info('    (NSGDD compatibility enabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')				-- WAS 500
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')		-- WAS 500
-		end
+		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
+		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
+		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
+		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
 
-		return
 	end
 
 	if vram == Var.vram.GB24 then
-		Cyberpunk.SetOption('World/Streaming/PersistencyCache', 'PoolBudgetKB', '16384')
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxBatchSize', '4000')			-- was 768 / vanilla 500
-		Cyberpunk.SetOption('Rendering', 'DistantShadowsMaxTrianglesPerBatch', '2147483647')	-- vanilla 400000
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxSize', '4000')  			-- was 768 vanilla 300
-		Cyberpunk.SetOption('Rendering', 'RainMapBatchMaxTrianglesPerBatch', '2147483647')  	-- vanilla 200000
-
-		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '128')
-		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '320')
+		Cyberpunk.SetOption('World', 'StreamingTeleportMagSq', '8192.000000')				-- vanilla 4096
+		Cyberpunk.SetOption('RayTracing', 'AccelerationStructureBuildNumMax', '128') 		
+		Cyberpunk.SetOption('RayTracing/DynamicInstance', 'UpdateProxyNumMax', '320') 		
 
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'FloodMinNonLoadingThreads', '3')
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'StreamMaxLoadingThreads', '3')
 		Cyberpunk.SetOption('ResourceLoaderThrottler', 'TrickleMaxLoadingThreads', '2')
 
-		if Var.settings.nsgddCompatible == false then
-			Logger.info('    (NSGDD compatibility disabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '10.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '20.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '500')				-- WAS 500
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '500')		-- WAS 500
-		else
-			Logger.info('    (NSGDD compatibility enabled)')
-			Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
-			Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
-			Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')				-- WAS 500
-			Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')		-- WAS 500
-		end
-
-		return
+		Cyberpunk.SetOption('Streaming', 'PrecacheDistance', '0.0')
+		Cyberpunk.SetOption('Streaming', 'MinStreamingDistance', '1.0')
+		Cyberpunk.SetOption('Streaming', 'MaxNodesPerFrame', '700')
+		Cyberpunk.SetOption('Streaming', 'EditorThrottledMaxNodesPerFrame', '700')
 	end
 end
 
